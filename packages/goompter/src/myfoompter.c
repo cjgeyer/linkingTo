037b034c5af6e0697bd3dc385d65bccea3615a01
Charles Geyer <charlie@stat.umn.edu>
2017-01-13 14:25:11 -0600

    another update

--- packages/goompter/src/myfoompter.c
+++ packages/goompter/src/myfoompter.c
@@ -3,10 +3,16 @@
 #include "myfoompter.h"
 #include "foompter.h"
 
-// the way this function works follows the examples in
+// Note that foompter.h is not in this package but in the foompter package
+// We can see it because we have foompter in the LinkingTo field of our
+// DESCRIPTION file and foompter has it in its inst/include directory.
+
+// The way this function works follows the examples in
 // the file Matrix_stubs.c in the R Recommended Package Matrix.
 // In any R source tree src/library/Recommended has the tarball.
 // After untarring it, the file is inst/include/Matrix_stubs.c.
+
+// The definitive reference is Section 5.4.2 of Writing R Extensions
 
 void myfoompter(int *n, double *x)
 {
