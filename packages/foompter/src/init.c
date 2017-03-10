c6c1cb3ba8b31296c3019d4c974d99b2796ab695
Charles J. Geyer <charlie@stat.umn.edu>
2017-03-09 19:49:39 -0600

    Remove styles from init.c
    They are deprecated in R-3.3.3 and Brian Ripley say
    get rid of them in all CRAN packages before R-3.4.0

--- packages/foompter/src/init.c
+++ packages/foompter/src/init.c
@@ -1,15 +1,17 @@
 
+// See Section 5.4 of Writing R Extensions
+//
+// Remove styles, which are deprecated in R-3.3.3
+
 #include <Rdefines.h>
 #include <R_ext/Rdynload.h>
 #include "foompter.h"
 
 static R_NativePrimitiveArgType foompter_types[2] = {INTSXP, REALSXP};
 
-static R_NativeArgStyle foompter_styles[4] = {R_ARG_IN, R_ARG_IN_OUT};
-
 static R_CMethodDef cMethods[] = {
-    {"foompter", (DL_FUNC) &foompter, 2, foompter_types, foompter_styles},
-    {NULL, NULL, 0, NULL, NULL}
+    {"foompter", (DL_FUNC) &foompter, 2, foompter_types},
+    {NULL, NULL, 0, NULL}
 };
 
 static R_CallMethodDef callMethods[]  = {
@@ -22,5 +24,4 @@ void R_init_foompter(DllInfo *info)
 
     R_RegisterCCallable("foompter", "foompter", (DL_FUNC) foompter);
 }
-
 
--- packages/goompter/src/init.c
+++ packages/goompter/src/init.c
@@ -1,15 +1,17 @@
 
+// See Section 5.4 of Writing R Extensions
+//
+// Remove styles, which are deprecated in R-3.3.3
+
 #include <Rdefines.h>
 #include <R_ext/Rdynload.h>
 #include "goompter.h"
 
 static R_NativePrimitiveArgType goompter_types[2] = {INTSXP, REALSXP};
 
-static R_NativeArgStyle goompter_styles[4] = {R_ARG_IN, R_ARG_IN_OUT};
-
 static R_CMethodDef cMethods[] = {
-    {"goompter", (DL_FUNC) &goompter, 2, goompter_types, goompter_styles},
-    {NULL, NULL, 0, NULL, NULL}
+    {"goompter", (DL_FUNC) &goompter, 2, goompter_types},
+    {NULL, NULL, 0, NULL}
 };
 
 static R_CallMethodDef callMethods[]  = {
@@ -20,5 +22,4 @@ void R_init_goompter(DllInfo *info)
 {
     R_registerRoutines(info, cMethods, callMethods, NULL, NULL);
 }
-
 
